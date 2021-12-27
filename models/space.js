999892d250d08376bed079140e96d01e493b92a2
Mariyathomasnld <jnmariya@gmail.com>
2021-12-27 16:23:13 +0100

    models and seed with relations

--- models/space.js
+++ models/space.js
@@ -7,9 +7,10 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
+     static associate(models) {
+      space.belongsTo(models.user);
+      space.hasMany(models.story)
+    };
   }
   space.init(
     {
--- seeders/20211227104611-some-spaces.js
+++ seeders/20211227104611-some-spaces.js
@@ -1,34 +1,31 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-   
-     await queryInterface.bulkInsert('spaces', [
-
-      {
-        title: "My time at Codaisseur",
-        description: "A tell all tale",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        
-      },
-      {
-        title: "I am a dummy",
-        backgroundColor: "#40C076",
-        color: "#EDEDED",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        
-      },
-     
-    
-    ], {});
-    
+    await queryInterface.bulkInsert(
+      "spaces",
+      [
+        {
+          userId: 1,
+          title: "My time at Codaisseur",
+          description: "A tell all tale",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2,
+          title: "I am a dummy",
+          backgroundColor: "#40C076",
+          color: "#EDEDED",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    
-    await queryInterface.bulkDelete('spaces', null, {});
-     
-  }
+    await queryInterface.bulkDelete("spaces", null, {});
+  },
 };
